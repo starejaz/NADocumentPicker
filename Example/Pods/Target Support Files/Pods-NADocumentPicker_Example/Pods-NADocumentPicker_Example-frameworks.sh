0e018eaf93c3ff0089d5f28f1034953400737de8
Nick Ager <nick.ager@gmail.com>
2017-07-12 09:43:54 +0100

    ran `pod update`
    which removed NSCommonUtils
    * added `platform 9.0` to `Example\Podfile`

--- Example/Pods/Target Support Files/Pods-NADocumentPicker_Example/Pods-NADocumentPicker_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-NADocumentPicker_Example/Pods-NADocumentPicker_Example-frameworks.sh
@@ -90,13 +90,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/BrightFutures/BrightFutures.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/NACommonUtils/NACommonUtils.framework"
   install_framework "$BUILT_PRODUCTS_DIR/NADocumentPicker/NADocumentPicker.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Result/Result.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/BrightFutures/BrightFutures.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/NACommonUtils/NACommonUtils.framework"
   install_framework "$BUILT_PRODUCTS_DIR/NADocumentPicker/NADocumentPicker.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Result/Result.framework"
 fi
